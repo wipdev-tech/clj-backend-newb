1e84ae6c057463a24aa1486825498eecb5944928
wip-dev <wipdev.tech@gmail.com>
2023-09-10 09:45:57 +0300

    hepatitis csv route

--- src/backend/core.clj
+++ src/backend/core.clj
@@ -4,22 +4,35 @@
             [ring.middleware.keyword-params :refer [wrap-keyword-params]]
             [ring.util.response :refer [content-type response]]
             [cheshire.core :refer [generate-string]]
-            [reitit.ring :as ring]))
+            [reitit.ring :as ring]
+            [clojure.data.csv :as csv]
+            [clojure.java.io :as io]
+            [backend.utils :as u]))
 
-(defn now [] (new java.util.Date))
-
-(defn handler [request]
-  (prn request)
+(defn ping-handler [request]
   (-> {:message (str "Hello "
                      (:name (:params request) "world")
                      "! You've successfully accessed the API!")
-       :at       (now)}
+       :at      (u/now)}
       generate-string
       response
       (content-type "application/json")))
 
+(defn hepatitis-handler [_]
+  (let [url "https://gist.githubusercontent.com/waseem-medhat/c5b5583f9423cf5f4fccf089395aa7f0/raw/hep_c.csv"]
+    (with-open [reader (io/reader url)]
+      (doall
+       (-> reader
+           csv/read-csv
+           u/csv-data->maps
+           (#(take 5 %))
+           generate-string
+           response
+           (content-type "application/json"))))))
+
 (def router
-  (ring/router ["/ping" {:get #(handler %)}]))
+  (ring/router [["/ping" {:get #(ping-handler %)}]
+                ["/hepatitis" {:get #(hepatitis-handler %)}]]))
 
 (def app (-> #((ring/ring-handler router) %)
              wrap-keyword-params
---
+++ src/backend/utils.clj
@@ -0,0 +1,11 @@
+(ns backend.utils)
+
+(defn now [] (new java.util.Date))
+
+(defn csv-data->maps [csv-data]
+  (map zipmap
+       (->> (first csv-data) ;; First row is the header
+            (map keyword) ;; Drop if you want string keys instead
+            repeat)
+       (rest csv-data)))
+
